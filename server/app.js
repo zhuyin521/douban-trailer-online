d58d56b261be914440f49cc8274e54dfe930be42
wq93 <214180117@qq.com>
2018-08-22 14:55:21 +0800

    使用puppeteer爬取豆瓣数据

--- server/app.js
+++ server/app.js
@@ -1,14 +1,7 @@
 const ejs = require('ejs')
 const Koa = require('koa')
 const app = new Koa()
-const {htmlTpl, ejsTpl} = require('./template')
-app.use(async (ctx, next) => {
-  ctx.type = 'text/html;chart=utf-8'
-  ctx.body = ejs.render(ejsTpl, {
-    you: 'Fang',
-    me: 'Qi'
-  })
-})
+
 
 app.listen(3333, () => {
   console.log('koa is running at 3333')
---
+++ server/crawler/trailer-list.js
@@ -0,0 +1,61 @@
+const puppeteer = require('puppeteer')
+
+const url = `https://movie.douban.com/tag/#/?sort=R&range=6,10&tags=%E7%94%B5%E5%BD%B1,%E6%BE%B3%E5%A4%A7%E5%88%A9%E4%BA%9A`
+
+const sleep = time => new Promise(resolve => {
+    setTimeout(resolve, time)
+  })
+
+;(async () => {
+  console.log('Start visit the target page')
+
+  const browser = await puppeteer.launch({
+    args: ['--no-sandbox'],
+    dumpio: false
+  })
+
+  const page = await browser.newPage()
+  await page.goto(url, {
+    waitUntil: 'networkidle2'
+  })
+
+  await sleep(3000)
+
+  await page.waitForSelector('.more') // 点击加载更多,爬取更多数据
+
+  for (let i = 0; i < 2; i++) {
+    await sleep(3000)
+    await page.click('.more')
+  }
+
+  const result = await page.evaluate(() => {
+    var $ = window.$ // 页面已加载好的jquery
+    var items = $('.list-wp a') // 列表的a标签
+    var links = []
+
+    if (items.length >= 1) {
+      items.each((index, item) => {
+        let it = $(item)
+        let doubanId = it.find('div').data('id')
+        let title = it.find('.title').text()
+        let rate = Number(it.find('.rate').text())
+        let poster = it.find('img').attr('src').replace('s_ratio', 'l_ratio')
+
+        links.push({
+          doubanId,
+          title,
+          rate,
+          poster
+        })
+      })
+    }
+
+    return links
+  })
+
+  browser.close()
+
+  console.log(result)
+  process.send({result})
+  process.exit(0)
+})()
